47de95c9cda93b17e51104ecfd6c49c8bdd54c66
Norrun <trulvens@tutanota.com>
2026-01-06 23:14:07 +0100

    still working on linguistics

--- constant.py
+++ constant.py
@@ -1,15 +1,15 @@
 from functools import reduce
 from trie import Trie
-#TODO: split up into combo_start combo_mid and combo_end.
+
 consonants_combo_begin = reduce(lambda x, y:x.add(y),[
-"bl","br","cl","cr","fl","fr","gl","gr"
+"bl","br","cl","cr","dr","fl","fr","gl","gr","pl","pr","sl","sm","sn","sp","st","str","shr","spr","scr","spl","thr","tr"
 ],
 Trie())
-consonants_combo_mid = reduce(lambda x, y:x.add(y),[
-"bl","br","cl","cr","fl","fr","gl","gr","nt"
+consonants_combo_mid = reduce(lambda x, y:x.add(y),[#  \/ Temporarly ignoring c and k (for the most part, mainly "sk")
+"bl","br","cl","cr","fl","fr","gl","gr","nt","pl","pr","sp","st","str","scr","spl","thr","tr"
 ], # Should double consonants be handled here or by a separate, but connected system? double consonants can end with a cluster.
 Trie())
 consonants_combo_end = reduce(lambda x, y:x.add(y),[
-"ct","nt"
+"ct","nt","sp","st"
 ],
 Trie())
