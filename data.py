6b1a57cf47f47fcc1afadd300f18bf94b8406b9d
Norrun <trulvens@tutanota.com>
2026-01-11 23:17:42 +0100

    wip

---
+++ data.py
@@ -0,0 +1,25 @@
+from functools import reduce
+from trie import Trie
+
+vowels = ["a", "e", "i", "o", "u"]
+vowel_combos = ["ai", "au", "ea", "ee", "ei", "eu", "ie", "oa", "oe", "oi", "oo", "ou", "ow", "oy", "ue", "ui"]
+consonants = [
+    "b", "c", "d", "f", "g", "h", "j", "k", "l", "m",
+    "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z",
+]
+double_consonants = ["b","d","f","g","l","m","n","p","r","s","t","z"]
+
+consonants_combo_begin = reduce(lambda x, y:x.add(y),[
+"bl","br","cl","cr","dr","dw","fl","fr","gl","gr","gw","pl","pr","qu","sl","sm","sn","sp","st","str","sw","shr","squ","spr","scr","spl",
+"thr","tr","tw"
+],
+Trie())
+consonants_combo_mid = reduce(lambda x, y:x.add(y),[#  \/ Temporarly ignoring c and k (for the most part, mainly "sk")
+"bl","br","cl","cr","fl","fr","gl","gr","nt","pl","pr","sp","st","str","scr","spl","thr","tr"
+], # Should double consonants be handled here or by a separate, but connected system? double consonants can end with a cluster.
+Trie())
+consonants_combo_end = reduce(lambda x, y:x.add(y),[# thinking about handeling "lv" "lg" "ls" "ng" ngl "n?" "nkl" "ps" "rs", by adding an e at the end in the Trie
+"bs","ct","ds","fs","ft","gs","ks","lb","lch","ld","lf","lk","lp","lt","mp","mph","mpt","nch","nd","ngs",
+"nk","nks","nt","pt","rb","rd","rk","rks","rl","rld","rm","rn","rp","rt","rth","sk","sp","st","ts","ths"
+],
+Trie())
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import secrets
+import data
+
+
+def generate(length):
+    word = ""
+    seq = [secrets.randbelow(2)]
+    trie_depth = 0
+    print(seq[0])
+    while length > 0:
+
+        if seq[-1] == 0:
+            if len(seq) < 2 or seq[-2] == 0 or length < 2:
+                word += secrets.choice(data.vowels)
+                length -= 1
+            elif secrets.randbelow(2):
+                word += secrets.choice(data.vowel_combos)
+                length -= 2
+        else:
+            pass
 
 
 def main():
-    pass
+    word = generate(10)
+    print(word)
 
 
 if __name__ == "__main__":
