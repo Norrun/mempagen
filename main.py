5ec2efcc5202330d60cd31943159241b1f1e8587
Norrun <trulvens@tutanota.com>
2026-01-13 23:44:39 +0100

    progress I guess

--- main.py
+++ main.py
@@ -10,14 +10,17 @@ def generate(length):
     while length > 0:
 
         if seq[-1] == 0:
-            if len(seq) < 2 or seq[-2] == 0 or length < 2:
+            if (len(seq) < 2 or seq[-2] == 0 or length < 2) and secrets.randbelow(2) == 1:
                 word += secrets.choice(data.vowels)
                 length -= 1
-            elif secrets.randbelow(2):
+            else:
                 word += secrets.choice(data.vowel_combos)
                 length -= 2
+            seq.append(1)
         else:
-            pass
+            random = secrets.randbelow(3)
+            if len(seq) < 2 or seq[-2] == 0 or length < 2:
+            
 
 
 def main():
