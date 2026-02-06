a0d28e558d0546131485013e5646890fdad737d7
Norrun <trulvens@tutanota.com>
2026-02-06 21:54:35 +0100

    sorted the values

--- data.py
+++ data.py
@@ -9,16 +9,16 @@ consonants = [
 double_consonants = ["b","d","f","g","l","m","n","p","r","s","t","z"]
 
 consonants_combo_begin2 = [
-"bl","br","cl","cr","dr","dw","fl","fr","gl","gr","gw","pl","pr","qu","sl","sm","sn","sp","st","str","sw","shr","squ","spr","scr","spl",
-"thr","tr","tw"
+"bl","br","cl","cr","dr","dw","fl","fr","gl","gr","gw","pl","pr","qu","sl","sm","sn","sp","st","sw",
+"tr","tw"
 ]
-consonants_combo_begin3 = [] #TODO: sort out from 2
+consonants_combo_begin3 = ["str","shr","squ","spr","scr","spl","thr",] 
 consonants_combo_mid2 = [#  \/ Temporarly ignoring c and k (for the most part, mainly "sk")
-"bl","br","cl","cr","fl","fr","gl","gr","nt","pl","pr","sp","st","str","scr","spl","thr","tr"
+"bl","br","cl","cr","fl","fr","gl","gr","nt","pl","pr","sp","st",
 ] # Should double consonants be handled here or by a separate, but connected system? double consonants can end with a cluster.
-consonants_combo_mid3 = [] #TODO: sort out from 2
+consonants_combo_mid3 = ["str","scr","spl","thr",] 
 consonants_combo_end2 = [# thinking about handeling "lv" "lg" "ls" "ng" ngl "n?" "nkl" "ps" "rs", by adding an e at the end in the Trie
-"bs","ct","ds","fs","ft","gs","ks","lb","lch","ld","lf","lk","lp","lt","mp","mph","mpt","nch","nd","ngs",
-"nk","nks","nt","pt","rb","rd","rk","rks","rl","rld","rm","rn","rp","rt","rth","sk","sp","st","ts","ths"
+"bs","ct","ds","fs","ft","gs","ks","lb","ld","lf","lk","lp","lt","mp","nd",
+"nk","nt","pt","rb","rd","rk","rl","rm","rn","rp","rt","sk","sp","st","ts",
 ]
-consonants_combo_end3 = [] #TODO: sort out from 2
+consonants_combo_end3 = ["lch","mph","mpt","nch","ngs","nks","rks","rld","rth","ths"] 
